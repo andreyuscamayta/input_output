05abf9bcee662551ab6a61babd4679d23ee031c4
Omar Mozo <andrey.uscamayta@prof.unifafibe.com.br>
2021-03-02 13:24:44 +0000

    change to read entire line into a string

--- parte1.c
+++ parte1.c
@@ -3,10 +3,10 @@
 int main(void) {
   printf("Bom dia :D, \nSeja bem-vindo ao canal de Atendimento da loja CYBER-FAFIBE\n");
   printf("Para poder começar a responder:\n \tPor favor indique o seu nome completo:\n\t\t");
-  char nome[50];
-  scanf("%s",nome);
+  char nome[256];
+  fgets(nome,sizeof nome,stdin);
   printf("\tPor Favor indicar seu telefone(somente os 11 numeros):\n\t\t");
-  char telefone[11];
+  char telefone[12];
   scanf("%s",telefone);
   printf("\n\tBem-vindo %s, com telefone: %s \n\tO link correspondente a este código está no seguinte link:\n",nome,telefone);  
   printf("\n\t#SCRATCH#%s\n","XXXXXXX");
